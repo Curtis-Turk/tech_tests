6e5ab72ba1a442b3ed8bacbf07d9f9c2a6fd11cf
Curtis Turk <curtis.c.turk@outlook.com>
2022-09-21 14:34:35 +0100

    Refactored first test and added second test

--- gilded-rose/test/gilded_rose.test.js
+++ gilded-rose/test/gilded_rose.test.js
@@ -1,9 +1,15 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
-  it("should be able to add an item", function () {
+  it("should be able to add an item", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(gildedRose.items[0].name).toBe("foo");
+    expect(gildedRose.items[0].sellIn).toBe(0);
+    expect(gildedRose.items[0].quality).toBe(0);
+  });
+  it("should decrease the quality of an item if updateQuality is called", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
   });
 });
