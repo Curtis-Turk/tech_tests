cdbb7a35ca39e47a517ddd8695b5fd3908c26659
Curtis Turk <curtis.c.turk@outlook.com>
2022-09-21 15:06:08 +0100

    Added some basic tests

--- gilded-rose/test/gilded_rose.test.js
+++ gilded-rose/test/gilded_rose.test.js
@@ -7,8 +7,36 @@ describe("Gilded Rose", function () {
     expect(gildedRose.items[0].sellIn).toBe(0);
     expect(gildedRose.items[0].quality).toBe(0);
   });
+  it("should be able to add multiple items", () => {
+    const items = [new Item("foo", 0, 0), new Item("bar", 0, 0)];
+    const gildedRose = new Shop(items);
+    expect(gildedRose.items[0].name).toBe("foo");
+    expect(gildedRose.items[1].name).toBe("bar");
+  });
   it("should decrease the quality of an item if updateQuality is called", () => {
     const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  });
+  it("should decrease the quality of an item if updateQuality is called twice", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 2)]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  });
+  it("should decrease the sellIn of an item if updateQuality is called", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].sellIn).toBe(0);
+  });
+  it("should decrease the sellIn of an item if updateQuality is called", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].sellIn).toBe(-1);
+  });
+  it("shouldn't decrease the quality of an item past 0", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
     gildedRose.updateQuality();
     expect(gildedRose.items[0].quality).toBe(0);
   });
